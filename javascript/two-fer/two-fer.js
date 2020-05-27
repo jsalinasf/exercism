13223837d1319dfb4ceb748c0d449524fb2eb6be
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-27 16:14:50 -0500

    two-fer and triangle boilerplate

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (firstName = 'you') => {
+  return `One for ${firstName}, one for me.`;
+  //throw new Error("Remove this statement and implement this function");
+};
