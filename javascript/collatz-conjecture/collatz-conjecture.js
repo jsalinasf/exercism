06678f70f61d59e8d94266cd4928e82c50b6d6ff
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-27 19:55:01 -0500

    Collatz Conjecture completed

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (number, counter = 0) => {
+  // if number is less or equal to zero, throw error
+  if (number <= 0) throw new Error('Only positive numbers are allowed');
+
+  // recursive condition to stop calling itself
+  if (number === 1) {
+    return counter;
+  } else {
+    // call itself until condition is met
+    // counter is used to count and pass the steps on every call
+    counter++;
+    if (number % 2 === 0) {
+      return steps(number / 2, counter);
+    } else {
+      return steps(3 * number + 1, counter);
+    }
+  }
 };
+
+// all tests have been passed
