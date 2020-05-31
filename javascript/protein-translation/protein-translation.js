a821b37fe0917ebb888e33c4f7775a5cd8ffc79f
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-30 23:50:01 -0500

    Protein Translator 90% - Missing stop condition

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -4,21 +4,41 @@
 //
 
 export const translate = (rna) => {
-  // Validate that received string has a complete number of condos
+  // initialize array to return
+  const protein = [];
+
+  // Edge Cases
+
+  // if received string is empty return empty array
+  if (!rna) return protein;
+
+  // if received string length is not a multiple of 3
+  // condos are incomplete thus otherwise throw error
   if (rna.length % 3 != 0) {
     throw new Error('Invalid codon');
   }
 
-  // Separate received string in subsets of 3
-  let protein = (splittedArray = rna.match(/.../g));
+  // Decode algorithm
 
-  // iterate array
+  // separate received string in subsets of 3
+  let splittedArray = rna.match(/.../g);
 
+  // iterate array of Codons
+  splittedArray.forEach((element) => {
+    if (TRANSLATION.hasOwnProperty(element)) {
+      // decode Condo, add it to protein array
+      protein.push(TRANSLATION[element]);
+    } else {
+      // Condo not found, throw error
+      throw new Error('Invalid codon');
+    }
+  });
   // return converted array
   return protein;
 };
 
 export const TRANSLATION = {
+  // Equivalency Table: Condos - Proteins
   AUG: 'Methionine',
   UUU: 'Phenylalanine',
   UUC: 'Phenylalanine',
@@ -37,3 +57,5 @@ export const TRANSLATION = {
   UAA: 'STOP',
   UAA: 'STOP',
 };
+
+console.log(translate('UUUUUC'));
