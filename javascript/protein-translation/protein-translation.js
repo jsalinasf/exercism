c914b1519a0ed36b15f5dfb75ea5e0a3bc06f41c
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-31 00:24:55 -0500

    Protein Translator completed

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -7,32 +7,38 @@ export const translate = (rna) => {
   // initialize array to return
   const protein = [];
 
-  // Edge Cases
+  // edge cases
 
   // if received string is empty return empty array
   if (!rna) return protein;
 
   // if received string length is not a multiple of 3
-  // condos are incomplete thus otherwise throw error
+  // condos are incomplete thus throw error
   if (rna.length % 3 != 0) {
     throw new Error('Invalid codon');
   }
 
-  // Decode algorithm
+  // algorithm
 
   // separate received string in subsets of 3
   let splittedArray = rna.match(/.../g);
 
-  // iterate array of Codons
-  splittedArray.forEach((element) => {
+  // iterate array of codons
+  for (let i = 0; i < splittedArray.length; i++) {
+    let element = splittedArray[i];
+    // does current condo have a match on the equivalency table?
     if (TRANSLATION.hasOwnProperty(element)) {
+      // is current condo a stop signal?
+      if (TRANSLATION[element] === 'STOP') {
+        return protein;
+      }
       // decode Condo, add it to protein array
       protein.push(TRANSLATION[element]);
     } else {
-      // Condo not found, throw error
+      // Condo not found on equivalency table, throw error
       throw new Error('Invalid codon');
     }
-  });
+  }
   // return converted array
   return protein;
 };
@@ -56,6 +62,8 @@ export const TRANSLATION = {
   UAA: 'STOP',
   UAA: 'STOP',
   UAA: 'STOP',
+  UGA: 'STOP',
+  UAG: 'STOP',
 };
 
-console.log(translate('UUUUUC'));
+console.log(translate('UAGAUGUAU'));
