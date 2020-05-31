6957ec181d673bb4ec2ddcb376b179b95eafe766
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-30 23:27:50 -0500

    Protein Translation 40% completed

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translate = (rna) => {
+  // Validate that received string has a complete number of condos
+  if (rna.length % 3 != 0) {
+    throw new Error('Invalid codon');
+  }
+
+  // Separate received string in subsets of 3
+  let protein = (splittedArray = rna.match(/.../g));
+
+  // iterate array
+
+  // return converted array
+  return protein;
+};
+
+export const TRANSLATION = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP',
+  UAA: 'STOP',
+  UAA: 'STOP',
 };
