e65515fe83409859ea9157c5dc925e3a47a7a99c
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-01 17:08:50 -0500

    Armonstrong numbers completed

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isArmstrongNumber = (num) => {
+  const arr = String(num).split('');
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += Math.pow(Number(arr[i]), arr.length);
+  }
+  if (num === sum) return true;
+  return false;
 };
