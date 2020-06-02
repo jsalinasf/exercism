229f4f5abd01c9339a235be8d87295eef8b0c995
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-01 19:53:56 -0500

    Length Encoding completed and passed all tests

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,5 +1,4 @@
-//export
-const encode = (str) => {
+export const encode = (str) => {
   let encodedString = '';
 
   // compare character n with character n+1 and subsequent ones using two loops
@@ -30,15 +29,23 @@ const encode = (str) => {
   return encodedString;
 };
 
-//export
-const decode = (str) => {
+// decode should work for numbers composed of more than 2 digits also
+export const decode = (str) => {
   let decodedString = '';
+  let repetitions = '';
 
   for (let i = 0; i < str.length; i++) {
     // is current character a number?
     if (str[i].match(NUMBERS)) {
+      if (repetitions.length < 2) repetitions = str[i];
+      // is next character also a number?
+      if (str[i + 1].match(NUMBERS)) {
+        repetitions += str[i + 1];
+        continue;
+      }
       // add (n+1) times (n) character to decodedString
-      decodedString += str[i + 1].repeat(str[i]);
+      decodedString += str[i + 1].repeat(Number(repetitions));
+      repetitions = '';
       // move loop index to next (n+1) character
       i++;
     } else {
@@ -46,16 +53,7 @@ const decode = (str) => {
       decodedString += str[i];
     }
   }
-
   return decodedString;
 };
 
-//export
-const NUMBERS = /^[0-9]+$/;
-
-console.log(decode(''));
-console.log(decode('XYZ'));
-console.log(decode('2A3B4C'));
-console.log(decode('12WB12W3B24WB'));
-console.log(decode('2 hs2q q2w2 '));
-console.log(decode('2a3b4c'));
+export const NUMBERS = /^[0-9]+$/;
