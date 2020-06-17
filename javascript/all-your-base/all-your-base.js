b441b15f241c5430647171b30c931e634a543f33
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-16 20:42:42 -0500

    Exercism - all-your-base progress 40%

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+//export
+const convert = (arr, originalBase, targetBase) => {
+  let returnArr = [];
+
+  if (targetBase === 10) {
+    let accumulator = 0;
+    for (let i = 0; i < arr.length; i++) {
+      let power = arr.length - 1 - i;
+      let sum = arr[i] * Math.pow(originalBase, power);
+      accumulator += sum;
+    }
+    returnArr = String(accumulator).split('');
+  }
+
+  return returnArr;
+
+  //throw new Error("Remove this statement and implement this function");
 };
+
+console.log(convert([1, 1, 1, 1, 1, 1, 1, 1], 2, 10)); //255
+console.log(convert([1, 1, 1, 0, 1], 2, 10)); // 29
--- javascript/all-your-base/all-your-base.spec.js
+++ javascript/all-your-base/all-your-base.spec.js
@@ -1,7 +1,7 @@
 import { convert } from './all-your-base';
 
 describe('Converter', () => {
-  test('single bit one to decimal', () => {
+  xtest('single bit one to decimal', () => {
     expect(convert([1], 2, 10)).toEqual([1]);
   });
 
@@ -17,7 +17,7 @@ describe('Converter', () => {
     expect(convert([1, 0, 1, 0, 1, 0], 2, 10)).toEqual([4, 2]);
   });
 
-  xtest('decimal to binary', () => {
+  test('decimal to binary', () => {
     expect(convert([4, 2], 10, 2)).toEqual([1, 0, 1, 0, 1, 0]);
   });
 
