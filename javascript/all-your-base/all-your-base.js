4b84e266ec0ea5cb76eacdaffa3afb5112583a2b
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-16 23:07:05 -0500

    Exercism - all-your-base 80% completed

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -4,17 +4,37 @@
 //
 
 //export
-const convert = (arr, originalBase, targetBase) => {
+const convert = (arr, sourceBase, targetBase) => {
   let returnArr = [];
 
-  if (targetBase === 10) {
+  function toDecimal(initialArray, fromBase) {
     let accumulator = 0;
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < initialArray.length; i++) {
       let power = arr.length - 1 - i;
-      let sum = arr[i] * Math.pow(originalBase, power);
+      let sum = arr[i] * Math.pow(fromBase, power);
       accumulator += sum;
     }
-    returnArr = String(accumulator).split('');
+    accumulator = String(accumulator).split('');
+    return accumulator;
+  }
+
+  function fromDecimal(initialArray, toBase) {
+    let accumulator = '';
+    let num = Number(initialArray.join(''));
+    while (num > 0) {
+      accumulator += num % toBase;
+      num = Math.trunc(num - num / toBase);
+    }
+    accumulator = accumulator.split('').reverse();
+    return accumulator;
+  }
+
+  if (sourceBase === 10) {
+    returnArr = fromDecimal(arr, targetBase);
+  }
+
+  if (targetBase === 10) {
+    returnArr = toDecimal(arr, sourceBase);
   }
 
   return returnArr;
@@ -24,3 +44,5 @@ const convert = (arr, originalBase, targetBase) => {
 
 console.log(convert([1, 1, 1, 1, 1, 1, 1, 1], 2, 10)); //255
 console.log(convert([1, 1, 1, 0, 1], 2, 10)); // 29
+console.log(convert([2, 2, 1], 3, 10)); // 25
+console.log(convert([4, 2], 10, 2)); // [1, 0, 1, 0, 1, 0]
